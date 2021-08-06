9567023fd68ff3dca712a90d6270aa1f5535d30a
Prem Deep <premdeepnawada@gmail.com>
2021-08-06 10:19:50 +0530

    Added BMI calculator code

--- calculator.js
+++ calculator.js
@@ -18,7 +18,16 @@ app.post("/", function(req, res){
 
   res.send("The result of calculation is " + result) ;
 });
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + '/bmiCalculator.html');
+});
+app.post("/bmicalculator", function(req, res){
+  var weight = parseFloat(req.body.wt) ;
+  var height = parseFloat(req.body.ht) ;
 
+  var result = weight/ (height*height) ;
+  res.send("Your BMI is " + result) ;
+})
 app.listen(3000, function(){
   console.log("Server started at port 3000");
 });
